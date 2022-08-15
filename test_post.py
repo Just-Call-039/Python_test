c603582f27f53c621e90e265986f843bba69ccb5
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2022-08-15 15:27:51 +0200

    Test post

--- test_post.py
+++ test_post.py
@@ -3,4 +3,18 @@ import requests
 index = input()
 
 adr = requests.get(f'https://www.postindexapi.ru/json/{index[:3]}/{index[:3]}{index[3:]}.json')
-print(adr.text)
+print(type(adr), adr)
+
+try:
+    full_adr = adr.json()
+    print(full_adr)
+    my_city = full_adr['City']
+    my_region = full_adr['Region']
+    if not my_city or my_city == '' or my_city == ' ':
+        my_city = 0
+    if not my_region or my_region == '' or my_region == ' ':
+        my_region = 0
+    print(my_city)
+    print(my_region)
+except (requests.exceptions.ConnectionError, ValueError):
+    print(0)
