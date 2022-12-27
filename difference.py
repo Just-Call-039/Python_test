f6554cc7e5404d9af452144e2a0707505b6d0a20
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2022-12-27 11:15:12 +0200

    Update difference.py

--- difference.py
+++ difference.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
-df_1 = pd.read_csv(r'C:\Users\Supervisor031\new.csv', sep=';')
-df_2 = pd.read_csv(r'C:\Users\Supervisor031\old.csv', sep=';')
+df_1 = pd.read_csv(r'\\10.88.22.128\dbs\4_report\Files\main_folder\2022_06.csv', sep=';', low_memory=False)
+df_2 = pd.read_csv(r'C:\Users\Supervisor031\2022_06.csv', sep=';', low_memory=False)
 
 # df_3 = df_1.merge(df_2, indicator=True, how='left', left_on='my_phone_work', right_on='my_phone_work').loc[
 #     lambda x: x['_merge'] != 'both']
-df_3 = df_1.merge(df_2, on='my_phone_work', how='outer', suffixes=['', '_'], indicator=True)
+df_3 = df_1.merge(df_2, on='id', how='outer', suffixes=['', '_'], indicator=True)
 
 # print(df_1.head())
-# print(df_1.info)
+print(df_1.info)
 # print(df_2.head())
-# print(df_2.info)
+print(df_2.info)
+print('-' * 50)
 print(df_3[df_3['_merge'] != 'both'])
 print('-' * 50)
 print(df_3.info)
