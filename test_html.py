380034394e02dcd5b95bc23f61707997db0ed23a
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-01-18 12:31:19 +0200

    test_html работает выгрузка файлов из папки.

--- test_html.py
+++ test_html.py
@@ -1,7 +1,14 @@
+import pandas as pd
+import os
+
 from bs4 import BeautifulSoup
 
 path = r'\\10.88.22.128\dbs\bson\Для Саши Б\test.xls'
-to_file = r'\\10.88.22.128\dbs\bson\Для Саши Б\test_out.csv'
+# to_file = r'\\10.88.22.128\dbs\bson\Для Саши Б\test_out.csv'
+# print(os.path.abspath('test.xls'))
+to_file = rf'\temp\{path.rstrip(".csv")}_temp.csv'
+# to_file_end = r'\\10.88.22.128\dbs\bson\Для Саши Б\test_end.csv'
+to_file_end = rf'\out\{path.rstrip(".csv")}_out.csv'
 
 with open(path, encoding='utf8') as f:
     soup = BeautifulSoup(f.read(), 'xml')
@@ -10,5 +17,18 @@ with open(to_file, 'w') as out:
     for now in soup.find_all('ss:Data'):
         temp = now.get_text()
         if any(True for i in (range(10)) if str(i) in temp) and len(temp) >= 10 and temp.isdigit() and '.' not in temp:
-            print(temp)
+            # print(temp)
             out.write(f'{temp}\n')
+
+df = pd.read_csv(to_file, header=None)
+# print(df.head())
+# print(df.info)
+# df = df.drop_duplicates(subset=0)
+df = df[0].unique()
+# print(len(df))
+df = pd.Series(df)
+df.to_csv(to_file_end, sep=';', header=False)
+
+os.remove(to_file)
+
+print('Done.')
--- test_html_2.py
+++ test_html_2.py
@@ -1,11 +1,51 @@
-# importing element tree
-import xml.etree.ElementTree as ET
+import pandas as pd
+import os
+import time
 
-path = r'\\10.88.22.128\dbs\bson\Для Саши Б\test.xls'
-tree = ET.parse(path)
+from bs4 import BeautifulSoup
+from commons.convert_time import convert_time
 
-root = tree.getroot()
+print(f'Время начала: {time.ctime()}')
+print()
 
-# print the root (parent) tag along with its memory location
-print(root)
-print(tree)
+path = r'\\10.88.22.128\dbs\bson\файлы xls'
+out = r'\\10.88.22.128\dbs\bson\файлы xls\out'
+temp = r'\\10.88.22.128\dbs\bson\файлы xls\temp'
+
+# print(*[i for i in os.listdir(path) if i.endswith('.xls' or '.xml')], sep='\n')
+list_files = os.listdir(path)
+
+for i in list_files:
+    start_time = time.time()
+    if i.endswith('.xls' or '.xml'):
+        full_path = f'{path}\\{i}'
+        temp_path = f'{temp}\\{i.rstrip(".xls")}_temp.csv'
+        out_path = f'{out}\\{i.rstrip(".xls")}_out.csv'
+
+        # print(full_path)
+        # print(temp_path)
+        # print(out_path)
+
+        with open(full_path, encoding='utf-8') as f:
+            soup = BeautifulSoup(f.read(), 'xml')
+
+        with open(temp_path, 'w', encoding='utf-8') as temp_file:
+            for now in soup.find_all('ss:Data'):
+                temp_text = now.get_text()
+                if any(True for i in (range(10)) if str(i) in temp_text) and len(temp_text) >= 10 \
+                        and temp_text.isdigit() and '.' not in temp_text:
+                    temp_file.write(f'{temp_text}\n')
+
+        df = pd.read_csv(temp_path, header=None)
+        df = df[0].unique()
+        df = pd.Series(df)
+        df.to_csv(out_path, sep=';', header=False)
+
+        # os.remove(to_file)
+
+        end_time = time.time()
+
+        print(f'На файл {out_path} ушло времени: {convert_time(end_time - start_time)}', end=' ')
+
+print()
+print(f'Время окончания: {time.ctime()}')
